946408960862009f2fca2a027a4c35ea06831836
JVcedraz <jvcarneiro@ecomp.uefs.br>
2023-04-17 23:08:19 -0300

    Update TechnicianDAO.java
    Creating method findByName

--- src/main/java/Aplication/DAO/TechnicianDao/TechnicianDAO.java
+++ src/main/java/Aplication/DAO/TechnicianDao/TechnicianDAO.java
@@ -6,4 +6,5 @@ import Aplication.DAO.CRUD;
 
 public interface TechnicianDAO extends CRUD<Technician> {
     public Technician create(Technician technician);
+    public Technician findByName(String name);
 }
