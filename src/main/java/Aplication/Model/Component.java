6f6ae2292d103934497bb41d59e4d8b5c6f7f0b7
JVcedraz <rggshrk@gmail.com>
2023-04-17 21:43:20 -0300

    Create Component.java
    Creating class component

---
+++ src/main/java/Aplication/Model/Component.java
@@ -0,0 +1,27 @@
+package Aplication.Model;
+
+public class Component {
+    Double cost;
+    Double price;
+    String name;
+
+    public Double getCost() {
+        return cost;
+    }
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
