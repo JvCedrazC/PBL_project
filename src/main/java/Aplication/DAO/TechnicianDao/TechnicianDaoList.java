fdd381539338c5d1fe382648eb5e29a4bcaf49ae
JVcedraz <jvcarneiro@ecomp.uefs.br>
2023-04-17 23:07:43 -0300

    Update TechnicianDaoList.java
    findMany does  not have parameters

--- src/main/java/Aplication/DAO/TechnicianDao/TechnicianDaoList.java
+++ src/main/java/Aplication/DAO/TechnicianDao/TechnicianDaoList.java
@@ -1,7 +1,75 @@
 package Aplication.DAO.TechnicianDao;
 
+
 import Aplication.Model.Technician;
 import Aplication.DAO.CRUD;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TechnicianDaoList implements CRUD<Technician>{
+    private List<Technician> lista;
+    private int nextID;
+
+    public TechnicianDaoList(){
+        this.lista = new ArrayList<Technician>();
+        this.nextID = 0;
+    }
+
+    @Override
+    public Technician create(Technician technician){
+        technician.setId(nextID);
+        nextID++;
+        this.lista.add(technician);
+        return technician;
+    }
+
+    @Override
+    public Technician findById(int id) {
+        for (Technician technician: this.lista) {
+            if (technician.getId() == id){
+                return technician;
+            }
+        } return null;
+
+    }
+    @Override
+    public List<Technician> findMany(){
+        List<Technician> listTechnician = new ArrayList<Technician>();
+        for (Object o : this.lista) {
+            listTechnician.add((Technician) o);
+        }
+        return listTechnician;
+    }
+
+    @Override
+    public void update(Technician technician){
+        for (int i = 0; i < this.lista.size(); i++) {
+            if (this.lista.get(i).getId() == technician.getId()) {
+                this.lista.set(i, technician);
+                return;
+            }
+        }
+    }
+    @Override
+    public void deleteById(int id){
+        for (int i = 0; i < this.lista.size(); i++) {
+            if (this.lista.get(i).getId() == id) {
+                this.lista.remove(i);
+                return;
+            }
+        }
+    }
+    public void deleteMany(){
+        this.lista = new ArrayList<Technician>();
+        this.nextID =  0;
+    }
+    public Technician findByName(String name){
+        for (Technician i: this.lista) {
+            if (i.getName() == name){
+                return i;
+            }
+        }return null;
+    }
 
 }
