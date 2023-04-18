67bc0813a99c948acc9b71bf519713547f17298a
JVcedraz <rggshrk@gmail.com>
2023-04-17 21:04:16 -0300

    Create Customer.java
    Creating Customer class

---
+++ src/main/java/Aplication/Model/Customer.java
@@ -0,0 +1,23 @@
+package Aplication.Model;
+
+public class Customer{
+    int id;
+    String name;
+
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+
+}
+
