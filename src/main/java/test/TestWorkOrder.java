c9ed9b246b7e3d826ea0998afe504756f5e47017
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-04-18 22:39:32 -0300

    Update TestWorkOrder.java

--- src/main/java/test/TestWorkOrder.java
+++ src/main/java/test/TestWorkOrder.java
@@ -1,7 +1,17 @@
 package test;
 
+import Aplication.DAO.WorkOrderDao.WorkOrderDAO;
+import Aplication.Model.WorkOrder;
+
 public class TestWorkOrder {
     public void testWk(){
-        return;
+        WorkOrder wk1 = new WorkOrder(0, "andamento",0.0, 0.0, 0, 0,"Pix");
+        WorkOrder wk2 = new WorkOrder(0, "andamento",0.0, 0.0, 0, 0,"Pix");
+        assertEquals(wk1, wk2);
     }
+
+
+}
+
+
 }
