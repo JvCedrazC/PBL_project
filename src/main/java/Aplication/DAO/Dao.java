166fd28a0b6bedc79f619add4b4762672c129579
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-04-18 22:39:46 -0300

    Update Dao.java

--- src/main/java/Aplication/DAO/Dao.java
+++ src/main/java/Aplication/DAO/Dao.java
@@ -3,14 +3,31 @@ package Aplication.DAO;
 import Aplication.DAO.CustomerDao.CustomerDAO;
 import Aplication.DAO.CustomerDao.CustomerDaoList;
 import Aplication.DAO.TechnicianDao.TechnicianDAO;
+import Aplication.DAO.TechnicianDao.TechnicianDaoList;
+import Aplication.DAO.WorkOrderDao.WorkOrderDAO;
+import Aplication.DAO.WorkOrderDao.WorkOrderDaoList;
+
 public class Dao{
     public static CustomerDAO customerDao;
     public static TechnicianDAO technicianDao;
+    public static WorkOrderDAO workOrderDAO;
     public static CustomerDAO getCustomer(){
         if (customerDao == null){
             customerDao = new CustomerDaoList();
         }
         return customerDao;
+    }
+    public static TechnicianDAO getTechnicianDao(){
+        if (technicianDao == null){
+            technicianDao = new TechnicianDaoList();
+        }
+        return technicianDao;
+    }
+    public static WorkOrderDAO workOrderDao(){
+        if (workOrderDAO == null){
+            workOrderDAO = new WorkOrderDaoList();
+        }
+        return workOrderDAO;
     }
 
 }
