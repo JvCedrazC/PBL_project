8ee04558d55efc4920827dbc0e3c5cc2d583fba9
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-04-18 22:53:26 -0300

    Update TestWorkOrder.java

--- src/main/java/test/TestWorkOrder.java
+++ src/main/java/test/TestWorkOrder.java
@@ -1,6 +1,7 @@
 package test;
 //import org.junit.jupiter.api.Test;
 import Aplication.DAO.WorkOrderDao.WorkOrderDAO;
+import Aplication.DAO.WorkOrderDao.WorkOrderDaoList;
 import Aplication.Model.WorkOrder;
 //import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,6 +12,10 @@ public class TestWorkOrder {
         //assertEquals(wk1, wk2);
     }
     public void testCriarWkDao(){
+        WorkOrderDAO dao = new WorkOrderDaoList();
+        WorkOrder wk1 = new WorkOrder(0, null, null, null, 0, 0, "pix");
+        dao.create(wk1);
+        assert dao.findById(0).getPaymentmethod() == wk1.getPaymentmethod(): "Erro ao criar novo dao";
 
     }
 
