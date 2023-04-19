a690da2a948042799752a06a152309a264912ca5
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-04-18 22:44:41 -0300

    Update TestWorkOrder.java

--- src/main/java/test/TestWorkOrder.java
+++ src/main/java/test/TestWorkOrder.java
@@ -1,17 +1,21 @@
 package test;
-
+//import org.junit.jupiter.api.Test;
 import Aplication.DAO.WorkOrderDao.WorkOrderDAO;
 import Aplication.Model.WorkOrder;
+//import static org.junit.jupiter.api.Assertions.*;
 
 public class TestWorkOrder {
     public void testWk(){
         WorkOrder wk1 = new WorkOrder(0, "andamento",0.0, 0.0, 0, 0,"Pix");
         WorkOrder wk2 = new WorkOrder(0, "andamento",0.0, 0.0, 0, 0,"Pix");
-        assertEquals(wk1, wk2);
+        //assertEquals(wk1, wk2);
+    }
+    public void testCriarWkDao(){
+
     }
 
 
 }
 
 
-}
+
