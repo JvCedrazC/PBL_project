dbc11da426c961155f49fb0b881d20fc988d1d6e
JVcedraz <jvcarneiro@ecomp.uefs.br>
2023-04-18 21:42:45 -0300

    Update WorkOrderDAO.java

--- src/main/java/Aplication/DAO/WorkOrderDao/WorkOrderDAO.java
+++ src/main/java/Aplication/DAO/WorkOrderDao/WorkOrderDAO.java
@@ -1,10 +1,12 @@
 package Aplication.DAO.WorkOrderDao;
 import Aplication.Model.Customer;
+import Aplication.Model.Technician;
 import Aplication.Model.WorkOrder;
 import Aplication.DAO.CRUD;
 
 public interface WorkOrderDAO extends CRUD<WorkOrder>{
     public WorkOrder create(WorkOrder workOrder);
     public WorkOrder findByCustomer(Customer customer);
-    
+    public WorkOrder findByTechnician(Technician technician);
+    public void deleteById(int id);
 }
